2c0c1461e2a0b0b92c24accac6625f0bf300fa17
Scala Steward <scala_steward@virtuslab.com>
2024-04-16 01:31:01 +0000

    Update zio-interop-cats to 23.1.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -80,7 +80,7 @@ object Dependencies {
     val http = "dev.zio" %% "zio-http" % "3.0.0-RC4"
     val schema = "dev.zio" %% "zio-schema" % "1.1.0"
     val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC23"
-    val catsInterop = "dev.zio" %% "zio-interop-cats" % "23.1.0.1"
+    val catsInterop = "dev.zio" %% "zio-interop-cats" % "23.1.0.2"
     val test = "dev.zio" %% "zio-test" % zioVersion
     val testSbt = "dev.zio" %% "zio-test-sbt" % zioVersion % Test
     val testMagnolia = "dev.zio" %% "zio-test-magnolia" % zioVersion % Test
