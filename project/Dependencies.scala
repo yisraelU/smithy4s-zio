4eaeb9f530091cd91b17a71c9ca937170a7df80a
Scala Steward <scala_steward@virtuslab.com>
2024-05-14 01:00:14 +0000

    Update zio-prelude to 1.0.0-RC26

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -79,7 +79,7 @@ object Dependencies {
     val core = "dev.zio" %% "zio" % zioVersion
     val http = "dev.zio" %% "zio-http" % "3.0.0-RC4"
     val schema = "dev.zio" %% "zio-schema" % "1.1.1"
-    val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC24"
+    val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC26"
     val catsInterop = "dev.zio" %% "zio-interop-cats" % "23.1.0.2"
     val test = "dev.zio" %% "zio-test" % zioVersion
     val testSbt = "dev.zio" %% "zio-test-sbt" % zioVersion % Test
