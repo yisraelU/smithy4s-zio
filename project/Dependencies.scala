bc8a8bc6a5a99f93d82dbd4c531033b5ce15bbc5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-05-28 21:42:21 +0200

    Update zio-prelude to 1.0.0-RC26 (#86)
    Co-authored-by: Yisrael Union <ysrlunion@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,9 +77,9 @@ object Dependencies {
   object ZIO {
     val zioVersion = "2.1.1"
     val core = "dev.zio" %% "zio" % zioVersion
+    val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC26"
     val http = "dev.zio" %% "zio-http" % "3.0.0-RC7"
     val schema = "dev.zio" %% "zio-schema" % "1.2.0"
-    val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC24"
     val catsInterop = "dev.zio" %% "zio-interop-cats" % "23.1.0.2"
     val test = "dev.zio" %% "zio-test" % zioVersion
     val testSbt = "dev.zio" %% "zio-test-sbt" % zioVersion % Test
