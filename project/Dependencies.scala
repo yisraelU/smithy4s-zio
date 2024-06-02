d0e81789855081a2253f8dbbec7eb72ed720bef2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-06-03 00:17:00 +0200

    Update zio-http to 3.0.0-RC8 (#91)
    Co-authored-by: Yisrael Union <ysrlunion@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,8 +77,9 @@ object Dependencies {
   object ZIO {
     val zioVersion = "2.1.1"
     val core = "dev.zio" %% "zio" % zioVersion
+
+    val http = "dev.zio" %% "zio-http" % "3.0.0-RC8"
     val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC27"
-    val http = "dev.zio" %% "zio-http" % "3.0.0-RC7"
     val schema = "dev.zio" %% "zio-schema" % "1.2.0"
     val catsInterop = "dev.zio" %% "zio-interop-cats" % "23.1.0.2"
     val test = "dev.zio" %% "zio-test" % zioVersion
