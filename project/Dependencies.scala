9ede4984a501244fe8e1711df71c347eccdd72ec
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-08-15 16:56:32 +0200

    Update fs2-data-xml to 1.11.1 (#119)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   }
   object Fs2Data {
     val xml: Def.Initialize[ModuleID] =
-      Def.setting("org.gnieh" %% "fs2-data-xml" % "1.11.0")
+      Def.setting("org.gnieh" %% "fs2-data-xml" % "1.11.1")
   }
   object LiHaoyi {
     val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.2.7"
