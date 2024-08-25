a879d07f4d7a12d4be5f6a2e75d7f381cd619f14
Scala Steward <scala_steward@virtuslab.com>
2024-08-25 21:44:37 +0000

    Update zio-test, zio-test-magnolia, ... to 2.1.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,7 +77,7 @@ object Dependencies {
   }
 
   object ZIO {
-    val zioVersion = "2.1.7"
+    val zioVersion = "2.1.8"
     val core = "dev.zio" %% "zio" % zioVersion
 
     val http = "dev.zio" %% "zio-http" % "3.0.0-RC9"
