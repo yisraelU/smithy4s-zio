a2179aa8d1223efab7d51d0894c467ad1e4d1f95
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-11-28 18:52:02 +0100

    Update smithy-aws-traits, smithy-build, ... to 1.53.0 (#166)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -58,7 +58,7 @@ object Dependencies {
   }
 
   object Smithy {
-    val smithyVersion = "1.52.1"
+    val smithyVersion = "1.53.0"
     val org = "software.amazon.smithy"
     val testTraits = org % "smithy-protocol-test-traits" % smithyVersion
     val model = org % "smithy-model" % smithyVersion
