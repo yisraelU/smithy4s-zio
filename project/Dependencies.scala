816ced8f0019442eed9639afec4915c996323c6a
Scala Steward <scala_steward@virtuslab.com>
2025-03-11 22:58:49 +0000

    Update zio-http to 3.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -80,7 +80,7 @@ object Dependencies {
     val zioVersion = "2.1.14"
     val core = "dev.zio" %% "zio" % zioVersion
 
-    val http = "dev.zio" %% "zio-http" % "3.0.1"
+    val http = "dev.zio" %% "zio-http" % "3.1.0"
     val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC35"
     val schema = "dev.zio" %% "zio-schema" % "1.5.0"
     val catsInterop = "dev.zio" %% "zio-interop-cats" % "23.1.0.3"
