f75d636a43c9c061bb17c0b309f7cb5dbc645643
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-03-27 21:44:34 +0100

    Update zio-prelude to 1.0.0-RC39 (#195)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -81,7 +81,7 @@ object Dependencies {
     val core = "dev.zio" %% "zio" % zioVersion
 
     val http = "dev.zio" %% "zio-http" % "3.0.1"
-    val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC35"
+    val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC39"
     val schema = "dev.zio" %% "zio-schema" % "1.5.0"
     val catsInterop = "dev.zio" %% "zio-interop-cats" % "23.1.0.5"
     val test = "dev.zio" %% "zio-test" % zioVersion
