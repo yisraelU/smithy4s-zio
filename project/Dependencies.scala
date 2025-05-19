f0c15b484a3afa9246690616c3b216c8678bd420
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-05-19 16:57:12 +0200

    Update zio-schema to 1.7.1 (#246)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -82,7 +82,7 @@ object Dependencies {
 
     val http = "dev.zio" %% "zio-http" % "3.0.1"
     val prelude = "dev.zio" %% "zio-prelude" % "1.0.0-RC40"
-    val schema = "dev.zio" %% "zio-schema" % "1.7.0"
+    val schema = "dev.zio" %% "zio-schema" % "1.7.1"
     val catsInterop = "dev.zio" %% "zio-interop-cats" % "23.1.0.5"
     val test = "dev.zio" %% "zio-test" % zioVersion
     val testSbt = "dev.zio" %% "zio-test-sbt" % zioVersion % Test
