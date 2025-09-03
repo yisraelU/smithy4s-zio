40a495700774e918ac23672d32f2bd977e279fc7
Scala Steward <scala_steward@virtuslab.com>
2025-09-03 20:34:47 +0000

    Update alloy-core to 0.3.33

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies {
 
   val Alloy = new {
     val org = "com.disneystreaming.alloy"
-    val alloyVersion = "0.3.32"
+    val alloyVersion = "0.3.33"
     val core = org % "alloy-core" % alloyVersion
     val openapi = org %% "alloy-openapi" % alloyVersion
     val `protocol-tests` = org % "alloy-protocol-tests" % alloyVersion
