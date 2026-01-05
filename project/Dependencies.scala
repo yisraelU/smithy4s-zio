4945af7457cdf06ca5ad2d07e3a48f1893c7ae03
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-01-05 16:28:19 +0100

    Update circe-parser to 0.14.15 (#314)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   }
 
   object Circe {
-    val version = "0.14.14"
+    val version = "0.14.15"
     val core = "io.circe" %% "circe-core" % version
     val parser = "io.circe" %% "circe-parser" % version
   }
